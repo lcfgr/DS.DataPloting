7e846332086c7ec61a2793fb6115b5af025c6c61
unknown <lampros@lcf.gr>
2015-08-09 19:45:10 +0200

    added files

---
+++ plot1.R
@@ -0,0 +1,37 @@
+## Load data.table package
+if (!require("data.table")) {
+	install.packages("data.table")
+	require("data.table")
+	}
+
+## Retrieve and prepare data file to be read
+fileURL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL,destfile='hpc.zip')
+unzip('hpc.zip')
+
+## Read Data file, filter it and create DateTimestamp in text form
+power_consumption <- fread('household_power_consumption.txt',na.strings=c("?"),sep=";")
+needed_power_consumption <- power_consumption [Date=="1/2/2007" | Date=="2/2/2007"]
+needed_power_consumption[,DateTime:=paste(Date,Time)]
+
+# Convert to numeric
+needed_power_consumption[,Global_active_power:=as.numeric(Global_active_power)]
+needed_power_consumption[,Global_reactive_power:=as.numeric(Global_reactive_power)]
+needed_power_consumption[,Voltage:=as.numeric(Voltage)]
+needed_power_consumption[,Sub_metering_1:=as.numeric(Sub_metering_1)]
+needed_power_consumption[,Sub_metering_2:=as.numeric(Sub_metering_2)]
+needed_power_consumption[,Sub_metering_3:=as.numeric(Sub_metering_3)]
+
+## create dataframe to add POSIXlt
+needed_power_consumption = as.data.frame(needed_power_consumption)
+needed_power_consumption$DateTime =strptime(needed_power_consumption$DateTime,format="%d/%m/%Y %H:%M:%S")
+
+
+# Plot histogram
+dev.new()
+par(bg="white")
+hist(needed_power_consumption$Global_active_power,col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+# Create png file with specific resolution
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
